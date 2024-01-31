269287599d599d7b0c7f3ee59e87c626291f1f33
Dhs <dhs@Dhss-MacBook-Air.local>
2024-01-31 10:19:36 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:discount_calc_new/common/strings.dart';
+import 'package:discount_calc_new/screens/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: Strings.appName,
+      home: HomePage(),
+    );
+  }
+}
