9a44bce13a05aea36cbdce7f1cc3f80e01f4ed1f
Dhs <dhruvashastri12@gmail.com>
2024-08-13 13:54:46 +0530

    Upgraded targetedsdk version  API level and ui colour changes

--- lib/utils/helper.dart
+++ lib/utils/helper.dart
@@ -1,11 +1,13 @@
+import 'package:flutter/material.dart';
+
 bool isValidNumber(double value) {
-  print('helper isValidNumber: $value');
+  debugPrint('helper isValidNumber: $value');
   return value > 0 && !value.isNegative;
 
 }
 
-bool isValidDiscountNumber(double value) {
-  print('helper isValidDiscount: $value');
-  return value > 0 && value < 101 && !value.isNegative;
+// bool isValidDiscountNumber(double value) {
+//   print('helper isValidDiscount: $value');
+//   return value > 0 && value < 100 && !value.isNegative;
   
-}
+// }
